497ca0d7b7b0d2bf338eb8fb85181242f351b6e6
siarhei-jazzteam <117805042+siarhei-jazzteam@users.noreply.github.com>
2023-01-26 14:40:17 +0300

    Add files via upload

---
+++ link.routes.js
@@ -0,0 +1,53 @@
+const { Router } = require('express');
+const config = require('config');
+const shortid = require('shortid');
+const Link = require('../models/Link');
+const auth = require('../middleWare/auth.middleware');
+const router = Router();
+
+// Будет генерировать сокращенную ссылку
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl');
+    const { from } = req.body;
+
+    const code = shortid.generate();
+
+    const existing = await Link.findOne({ from }); //проверка есть ли такая ссылка в базе
+
+    if (existing) {
+      return res.json({ link: existing });
+    }
+
+    const to = baseUrl + '/t/' + code;
+
+    const link = new Link({ code, to, from, owner: req.user.userId });
+
+    await link.save();
+    res.status(201).json({ link });
+  } catch (err) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+// Запрос для получения всех ссылок пользователя
+router.get('/', auth, async (req, res) => {
+  try {
+    const links = await Link.find({ owner: req.user.userId });
+    res.json(links);
+  } catch (err) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+// Для получения ссылки по id
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const link = await Link.findById(req.params.id);
+    res.json(link);
+  } catch (err) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+module.exports = router;
